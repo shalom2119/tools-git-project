b449035de64a52f6c0c24de65c69bb3fe2e823a5
samson <samsonleul@gmail.com>
2022-02-07 10:03:49 -0500

    Update meanState.java

--- src/main/java/com/example/statgui/state/meanState.java
+++ src/main/java/com/example/statgui/state/meanState.java
@@ -4,5 +4,7 @@ import com.example.statgui.controller.calculator;
 import javafx.scene.chart.XYChart;
 
 public class meanState extends State {
+    public meanState(calculator handle) {
+        super(handle);
 
 }
