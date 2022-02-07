7cfaef635fdf58330511efb893c76f2c9e1556ac
samson <samsonleul@gmail.com>
2022-02-07 11:12:20 -0500

    Update meanState.java

--- src/main/java/com/example/statgui/state/meanState.java
+++ src/main/java/com/example/statgui/state/meanState.java
@@ -8,10 +8,18 @@ public class meanState extends State {
         super(handle);
 }
 @Override
-    public void calculate() {
-        public meanState(calculator handle) {
-            super(handle);
+public void calculate() {
 
-        
-    }
+    int counter = 0;
+    float sum = 0;
+    float mean = 0;
+        for (XYChart.Data<String, Number> data : handle.getDataSet().getData()) {
+            sum += data.getYValue().floatValue();
+            counter++;
+        }
+    if(counter >0)
+        mean = sum/counter;
+
+    handle.paramValue.setText(String.valueOf(mean));
+}
 }
