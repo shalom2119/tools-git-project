6f09ecb6c4d3a404bce63162147fd72c295a1942
samson <samsonleul@gmail.com>
2022-02-07 10:01:12 -0500

    Revert "Update meanState.java"
    This reverts commit b0fcf14d6875796d8584226896059cfb09832377.

--- src/main/java/com/example/statgui/state/meanState.java
+++ src/main/java/com/example/statgui/state/meanState.java
@@ -4,23 +4,5 @@ import com.example.statgui.controller.calculator;
 import javafx.scene.chart.XYChart;
 
 public class meanState extends State {
-    public meanState(calculator handle) {
-        super(handle);
-    }
 
-    @Override
-    public void calculate() {
-
-        int counter = 0;
-        float sum = 0;
-        float mean = 0;
-            for (XYChart.Data<String, Number> data : handle.getDataSet().getData()) {
-                sum += data.getYValue().floatValue();
-                counter++;
-            }
-        if(counter >0)
-            mean = sum/counter;
-
-        handle.paramValue.setText(String.valueOf(mean));
-    }
 }
