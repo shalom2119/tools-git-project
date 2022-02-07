840b91638092112450f2d4c984cc6de5c7c0b8a1
samson <samsonleul@gmail.com>
2022-02-07 10:10:56 -0500

    Update modeState.java

--- src/main/java/com/example/statgui/state/modeState.java
+++ src/main/java/com/example/statgui/state/modeState.java
@@ -5,6 +5,18 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class modeState extends State {
+    public modeState(calculator handle) {
+        super(handle);
+    }
 
+    @Override
+    public void calculate() {
+
+        ArrayList<Float> list = handle.getDataArray();
+
+        int frequencyList[] = new int[list.size()];
+        int counter = 0;
+        int mode = 0;
+        StringBuilder modelist = new StringBuilder();
 
 }
