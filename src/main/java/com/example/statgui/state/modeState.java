4db32c7e90642ee434761eb0a2de40a64bd62241
szm2119 <szmwg2119@gmail.com>
2022-02-07 12:24:16 +0300

    counter replaced with 5

--- src/main/java/com/example/statgui/state/modeState.java
+++ src/main/java/com/example/statgui/state/modeState.java
@@ -31,7 +31,7 @@ public class modeState extends State {
                             list.set(j, null);
                         }
                     }
-                    frequencyList[i] = counter;
+                    frequencyList[i] = 5;
                 }
             }
 
