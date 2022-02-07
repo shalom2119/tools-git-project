710b106b1a9ddf2392e89440cf2cabb6b907e8b2
samson <samsonleul@gmail.com>
2022-02-07 11:14:20 -0500

    Update modeState.java

--- src/main/java/com/example/statgui/state/modeState.java
+++ src/main/java/com/example/statgui/state/modeState.java
@@ -18,7 +18,24 @@ public class modeState extends State {
         int counter = 0;
         int mode = 0;
         StringBuilder modelist = new StringBuilder();
+        if(list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
+                counter = 1;
+                if (list.get(i) == null) {
+                    frequencyList[i] = 0;
+                } else {
 
+                    for (int j = i + 1; j < list.size(); j++) {
+                        if (Objects.equals(list.get(i), list.get(j))) {
+                            counter++;
+                            list.set(j, null);
+                        }
+                    }
+                    frequencyList[i] = counter;
+                }
+            }
+
+            mode = frequencyList[0];
         
         }
     }
