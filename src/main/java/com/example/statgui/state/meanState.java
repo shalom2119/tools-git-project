316dc819f6c6acda997c007d66c7f558f95fc249
samson <samsonleul@gmail.com>
2022-02-07 11:10:17 -0500

    Revert "Update meanState.java"
    This reverts commit 55783b7438e6b1a3fd73640bf53a7cf95f512b89.

--- src/main/java/com/example/statgui/state/meanState.java
+++ src/main/java/com/example/statgui/state/meanState.java
@@ -4,7 +4,8 @@ import com.example.statgui.controller.calculator;
 import javafx.scene.chart.XYChart;
 
 public class meanState extends State {
-    
+    public meanState(calculator handle) {
+        super(handle);
 }
 @Override
     public void calculate() {
