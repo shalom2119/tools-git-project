9972ae3127e2518cd0be0ea390d795b048318bf0
samson <samsonleul@gmail.com>
2022-02-07 10:27:12 -0500

    Update State.java

--- src/main/java/com/example/statgui/state/State.java
+++ src/main/java/com/example/statgui/state/State.java
@@ -4,5 +4,10 @@ import com.example.statgui.controller.calculator;
 
 public abstract class State {
     protected calculator handle;
+    public State(calculator handle) {
+        this.handle = handle;
+    }
+
+    public abstract void calculate();
 
 }
