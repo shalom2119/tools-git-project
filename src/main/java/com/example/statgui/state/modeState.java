3c7584c28e6936177de76370eaf9e134d606bbc7
szm2119 <szmwg2119@gmail.com>
2022-02-07 13:46:45 +0300

    reset states

--- src/main/java/com/example/statgui/state/modeState.java
+++ src/main/java/com/example/statgui/state/modeState.java
@@ -9,36 +9,6 @@ public class modeState extends State {
         super(handle);
     }
 
-    @Override
-    public void cculate() {
-
-        ArrayList<Float> list = handle.getDataArray();
-
-        int frequencyList[] = new int[list.size()];
-        int counter = 0;
-        int mode = 0;
-        StringBuilder modelist = new StringBuilder();
-        if(list.size() > 0) {
-            for (int i = 0; i < list.size(); i++) {
-                counter = 1;
-                if (list.get(i) == null) {
-                    frequencyList[i] = 0;
-                } else {
-
-                    for (int j = i + 1; j < list.size(); j++) {
-                        if (Objects.equals(list.get(i), list.get(j))) {
-                            counter++;
-                            list.set(j, null);
-                        }
-                    }
-                    frequencyList[i] = counter;
-                }
-            }
-
-            
-            paramValue.setText(String.valueOf(modelist+"   some text
-            "));
-        
-        }
-    }
+    
+    
 }
