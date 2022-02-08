e9e8ec834dc76c94a822ad4fa00c560425d4ea55
szm2119 <76486857+shalom2119@users.noreply.github.com>
2022-02-08 14:24:23 +0300

    update modestate.java with "mean" code

--- src/main/java/com/example/statgui/state/modeState.java
+++ src/main/java/com/example/statgui/state/modeState.java
@@ -9,6 +9,20 @@ public class modeState extends State {
         super(handle);
     }
 
-    
+     @Override
+    public void calculate() {
+
+        int counter = 0;
+        float sum = 0;
+        float mean = 0;
+            for (XYChart.Data<String, Number> data : handle.getDataSet().getData()) {
+                sum += data.getYValue().floatValue();
+                counter++;
+            }
+        if(counter >0)
+            mean = sum/counter;
+
+        handle.paramValue.setText(String.valueOf(mean));
+    }
     
 }
