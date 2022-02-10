89aa0296d91795e6374fae029b837b86d0dda8a4
Yohannes Fenta <yohannesfenta12@gmail.com>
2022-02-10 11:12:37 +0300

    Server class is add to do caluclations from clients

---
+++ src/main/java/com/example/statgui/RMI/Server.java
@@ -0,0 +1,19 @@
+package sample;
+
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+public class Server {
+    public static void main(String[] args) {
+        try{
+            CalculatorRemote calc = new CalculatorRemote();
+            LocateRegistry.createRegistry(8000);
+            Naming.rebind("rmi://localhost:8000/remoteCalculator",calc);
+            System.out.println("server running");
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
+    }
+
+}
