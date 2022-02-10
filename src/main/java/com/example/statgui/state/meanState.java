169b260ae78d8ec2061b0322e5fbfbb331a745db
Yohannes Fenta <yohannesfenta12@gmail.com>
2022-02-10 09:37:10 +0300

    Button package is imported on meanState

--- src/main/java/com/example/statgui/state/meanState.java
+++ src/main/java/com/example/statgui/state/meanState.java
@@ -2,6 +2,7 @@ package com.example.statgui.state;
 
 import com.example.statgui.controller.calculator;
 import javafx.scene.chart.XYChart;
+import javafx.button.Button;
 
 public class meanState extends State {
 
